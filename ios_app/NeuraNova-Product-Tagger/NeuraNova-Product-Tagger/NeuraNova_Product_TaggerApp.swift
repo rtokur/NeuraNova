46c6f13fb88502765947364ef268aee75ff00f0d
Rümeysa Tokur <rtokur11@gmail.com>
2025-07-24 23:48:36 +0300

    Commit

---
+++ ios_app/NeuraNova-Product-Tagger/NeuraNova-Product-Tagger/NeuraNova_Product_TaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuraNova_Product_TaggerApp.swift
+//  NeuraNova-Product-Tagger
+//
+//  Created by Rumeysa Tokur on 24.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NeuraNova_Product_TaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
