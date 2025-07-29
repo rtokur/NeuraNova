f8515589e524af7cb4f0e50780c17595db192643
Rümeysa Tokur <rtokur11@gmail.com>
2025-07-29 17:24:04 +0300

    Add UIKit iOS project structure

---
+++ ios_app/NeuraNova-Product-Tagger/NeuraNova-Product-Tagger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeuraNova-Product-Tagger
+//
+//  Created by Rumeysa Tokur on 29.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
